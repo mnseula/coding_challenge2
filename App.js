c90092879916c138fe00b0ceda052b7367a3dc5d
mnseula <mikenseula@gmail.com>
2022-04-27 16:37:53 -0400

    Create App.js
    The main file of my bord.

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ant Moves</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
